9955f98430b77eefa660e16f71d5f35a8e19a13e
Rafał Gierczak <grime85@gmail.com>
2017-04-01 00:42:35 +0200

    Added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
     imports: [
+        AppRoutingModule,
         BrowserModule.withServerTransition({appId: 'cli-universal-demo'}),
         FormsModule,
         HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        ContactComponent,
+        HomeComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
