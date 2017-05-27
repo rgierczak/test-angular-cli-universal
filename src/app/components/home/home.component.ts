106ba3e2290ff15ee3200db07e923b6ce016ba23
Rafał Gierczak <grime85@gmail.com>
2017-05-27 13:48:39 +0200

    Added simple animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    trigger,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    animations: [
+        trigger('opacityState', [
+            transition(':enter', [
+                style({
+                    opacity: 0
+                }),
+                animate(300)
+            ]),
+            transition(':leave', animate(300, style({
+                opacity: 0
+            })))
+        ])
+    ]
 })
 export class HomeComponent implements OnInit {
+    constructor() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
